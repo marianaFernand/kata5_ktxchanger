786c5b2e62c011580a480e7d505ca55edc476e19
mariana fernandez garcia <marianafernandezgarcia@gmail.com>
2019-07-18 15:28:03 +0200

    llamadas con requiest

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 from tkinter import *
 from tkinter import ttk
+
+
+import configparser
+import json
+import requests
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+inSymbol = input ("qué moneda quieres convertir: ")
+outSymbol = input ("en qué otra moneda: ")
+
+url = config["fixer.io"]["RATE_LATEST_EP"]
+api_key = config["fixer.io"]["API_KEY"]
+
+url = url.format(api_key, inSymbol, outSymbol)
+response = requests.get(url)
+if response.status_code ==200:
+    print (response.text)
+else:
+    print ("se ha producido un error en la petición", response.status_code)
+
+
+
